f54613680c96eddda946141afb3cb6885e674747
Dan Liu <d1liu@ieng6-202.ucsd.edu>
2020-06-19 01:14:43 -0700

    second draft of DREAM implementation

--- client/main.c
+++ client/main.c
@@ -1,7 +1,6 @@
 // Client side
 #include <stdio.h> 
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <unistd.h> 
@@ -11,38 +10,66 @@
 #include <stdbool.h>
 #include <sys/stat.h>
 #define PORT 3000 
+
+#include <netinet/in.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
+
+
+void doOffload( int sockID, int fileID ){
+    // not yet sure if we actually want anything in here
+    // offloading technique is unknown yet
+    printf("Offload\n");
+}
+
+void doLocal( int fileID ){
+    // not implemented yet
+    printf("DO it local\n");
+}
+
+
+
 //passed in complexity
 //in the first draft:
 //assume every quest has greater than O(n) complexity
 //try to offload every task
 void *requestOffload(int sockID, int complexity){
     while(true){
+        // I am thinking about switching this to a posix timer if this does not         // work as expected.
         sleep(10);
 
         //get file information
         char file[14];
         char data[1024];
+        char instruction[3];
         //random file
         int randomFile = rand() % 10;
         snprintf(file,14, "Videos/Video%d", randomFile);
+
         struct stat st;
         stat(file, &st);
         int sizeOfFile = st.st_size;
-        snprintf(data,1024,"%d,%d", complexity, sizeOfFile);
-        send(sockID, data, 1024, 0);
 
+        snprintf(data,1024,"%d,%d,10", complexity, sizeOfFile);
+        send(sockID, data, 1024, 0);
+        printf("Random file %d descriptor sent\n", randomFile);
+        read(sockID, instruction, 3);
+        printf("Permission received: %s\n", instruction);
+        if(strcmp(instruction,"yes") == 0){
+            doOffload(sockID, randomFile);
+        }
+        else{
+            doLocal(randomFile);
+        }
     }
 }
 
 
+
 int main(int argc, char const *argv[]) 
 { 
-
-    //socket initialization
-    //attribute to https://www.geeksforgeeks.org/socket-programming-cc/
-    int sock = 0, valread; 
+    int sock = 0; 
     struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
     char buffer[1024] = {0}; 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
@@ -64,9 +91,10 @@ int main(int argc, char const *argv[])
     { 
         printf("\nConnection Failed \n"); 
         return -1; 
-    } 
-
-
+    }
+    char greeting[1024];
+    read(sock, greeting, 1024);
+    printf("Greeting message received: %s\n", greeting);
 /*---------------------------------------------------------------*/
 //thread to communicate every ten seconds
     pthread_t work;
@@ -77,5 +105,8 @@ int main(int argc, char const *argv[])
     valread = read( sock , buffer, 1024); 
     printf("%s\n",buffer ); 
     */
-    return 0; 
-} 
+    close(sock);
+    pthread_exit(NULL);
+
+}
+
---
+++ server/main.c
@@ -0,0 +1,237 @@
+// code attributed to https://www.geeksforgeeks.org/socket-programming-in-cc-handling-multiple-clients-on-server-without-multi-threading/
+// server
+//Example code: A simple server side code, which echos back the received message. 
+//Handle multiple socket connections with select and fd_set on Linux  
+#include <stdio.h>  
+#include <string.h>   //strlen  
+#include <stdlib.h>  
+#include <errno.h>  
+#include <unistd.h>   //close  
+#include <arpa/inet.h>    //close  
+#include <sys/types.h>  
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros  
+#define TRUE   1  
+#define FALSE  0  
+#define PORT 3000  
+
+typedef struct information{
+    int complexity;
+    unsigned int size;
+    double max_delay;
+}info;
+
+int main(int argc , char *argv[])   
+{   
+    int opt = TRUE;   
+    int master_socket , addrlen , new_socket , client_socket[30] ,  
+          max_clients = 30 , activity, i , valread , sd;   
+    int max_sd;   
+    struct sockaddr_in address;   
+         
+    char buffer[1025];  //data buffer of 1K  
+         
+    //set of socket descriptors  
+    fd_set readfds;   
+         
+    //a message  
+    char *message = "DREAM IMPLEMENTATION v1.0 \r\n";   
+     
+    //initialise all client_socket[] to 0 so not checked  
+    for (i = 0; i < max_clients; i++)   
+    {   
+        client_socket[i] = 0;   
+    }   
+         
+    //create a master socket  
+    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)   
+    {   
+        perror("socket failed");   
+        exit(EXIT_FAILURE);   
+    }   
+     
+    //set master socket to allow multiple connections ,  
+    //this is just a good habit, it will work without this  
+    if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,  
+          sizeof(opt)) < 0 )   
+    {   
+        perror("setsockopt");   
+        exit(EXIT_FAILURE);   
+    }   
+     
+    //type of socket created  
+    address.sin_family = AF_INET;   
+    address.sin_addr.s_addr = INADDR_ANY;   
+    address.sin_port = htons( PORT );   
+         
+    //bind the socket to localhost port 8888  
+    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)   
+    {   
+        perror("bind failed");   
+        exit(EXIT_FAILURE);   
+    }   
+    printf("Listener on port %d \n", PORT);   
+         
+    //try to specify maximum of 3 pending connections for the master socket  
+    if (listen(master_socket, 3) < 0)   
+    {   
+        perror("listen");   
+        exit(EXIT_FAILURE);   
+    }   
+         
+    //accept the incoming connection  
+    addrlen = sizeof(address);   
+    puts("Waiting for connections ...");   
+
+
+    // queue on server to decide whether a task should be accepted.
+    // push into the vector means the work will be on the server, and the server
+    // will respond "yes". Then the client will do a O(n) work to simulate 
+    // offloading and server will do O(n^2) work. 
+    // Otherwise it will respond with "no".
+    // Then client will do a O(n^2) work to simulate process on local.
+    double current_work_time = 0.0;
+    // this should really be a vector, but c does not have vector
+    // for nnow, just use an array, I will update this if we are all right on other things
+    info queue[30];
+
+    while(TRUE)   
+    {   
+        //clear the socket set  
+        FD_ZERO(&readfds);   
+     
+        //add master socket to set  
+        FD_SET(master_socket, &readfds);   
+        max_sd = master_socket;   
+             
+        //add child sockets to set  
+        for ( i = 0 ; i < max_clients ; i++)   
+        {   
+            //socket descriptor  
+            sd = client_socket[i];   
+                 
+            //if valid socket descriptor then add to read list  
+            if(sd > 0)   
+                FD_SET( sd , &readfds);   
+                 
+            //highest file descriptor number, need it for the select function  
+            if(sd > max_sd)   
+                max_sd = sd;   
+        }   
+     
+        //wait for an activity on one of the sockets , timeout is NULL ,  
+        //so wait indefinitely  
+        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);   
+       
+        if ((activity < 0) && (errno!=EINTR))   
+        {   
+            printf("select error");   
+        }   
+             
+        //If something happened on the master socket ,  
+        //then its an incoming connection  
+        if (FD_ISSET(master_socket, &readfds))   
+        {   
+            if ((new_socket = accept(master_socket,  
+                    (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)   
+            {   
+                perror("accept");   
+                exit(EXIT_FAILURE);   
+            }   
+             
+            //inform user of socket number - used in send and receive commands  
+            printf("New connection , socket fd is %d , ip is : %s , port : %d\n", new_socket , inet_ntoa(address.sin_addr), ntohs(address.sin_port));   
+           
+            //send new connection greeting message  
+            if( send(new_socket, message, strlen(message), 0) != strlen(message) )   
+            {   
+                perror("send");   
+            }   
+                 
+            puts("Welcome message sent successfully");   
+                 
+            //add new socket to array of sockets  
+            for (i = 0; i < max_clients; i++)   
+            {   
+                //if position is empty  
+                if( client_socket[i] == 0 )   
+                {   
+                    client_socket[i] = new_socket;   
+                    printf("Adding to list of sockets as %d\n" , i);   
+                         
+                    break;   
+                }   
+            }   
+        }   
+             
+        //else its some IO operation on some other socket 
+        for (i = 0; i < max_clients; i++)   
+        {   
+            sd = client_socket[i];   
+                 
+            if (FD_ISSET( sd , &readfds))   
+            {   
+                //Check if it was for closing , and also read the  
+                //incoming message  
+                if ((valread = read( sd , buffer, 1024)) == 0)   
+                {   
+                    //Somebody disconnected , get his details and print  
+                    getpeername(sd , (struct sockaddr*)&address, (socklen_t*)&addrlen);   
+                    printf("Host disconnected , ip %s , port %d\n", inet_ntoa(address.sin_addr) , ntohs(address.sin_port));   
+                         
+                    //Close the socket and mark as 0 in list for reuse  
+                    close( sd );   
+                    client_socket[i] = 0;   
+                }   
+                // change this part
+                /*
+                //Echo back the message that came in  
+                else 
+                {   
+                    //set the string terminating NULL byte on the end  
+                    //of the data read  
+                    buffer[valread] = '\0';   
+                    send(sd , buffer , strlen(buffer) , 0 );   
+                }   
+                */
+                else{
+                    if(current_work_time > 0.5 ){
+
+                        // so this is supposed to be a thread thing where
+                        // it executes the offloaded stuff, but since we are not
+                        // measuring the energy, just sleep and 
+                        // decrease the number
+                        sleep(0.5);
+                        current_work_time -=0.5;
+                    }
+                    int complexity = atoi(strtok(buffer, ","));
+                    unsigned int size = (unsigned int)atoi(strtok(NULL, ","));
+                    double delay = atof(strtok(NULL, ","));
+                    printf("delay is: %f\n", delay);
+                    info newQuest;
+                    newQuest.complexity = complexity;
+                    newQuest.size = size;
+                    newQuest.max_delay = delay;
+                    double estimateTime = ((double)(size*size))/500000000.0;
+                    //500M
+                    printf("Estimate time for the task is: %f\n",estimateTime);
+                    // this needs actual data to support
+                    if(delay > (estimateTime+current_work_time)){
+                        send(sd, "yes", 3, 0);
+                        // add to the list
+                        queue[i] = newQuest;
+                        current_work_time += estimateTime;
+                        printf("current total time for work is: %f\n", current_work_time);
+                    }
+                    else{
+                        // do not add
+                        send(sd, "No!", 3, 0);
+                    }
+                }
+            }   
+        }   
+    }   
+         
+    return 0;   
+}   
